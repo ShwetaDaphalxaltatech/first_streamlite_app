c434799c953d559bda2929531f39777d5e3951ef
ShwetaDaphalxaltatech <140041919+ShwetaDaphalxaltatech@users.noreply.github.com>
2023-11-06 10:39:34 +0530

    Update first_streamlit_app.py
    update code

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -22,12 +22,12 @@ streamlit.header("Fruityvice Fruit Advice!")
 ruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 # streamlit.text(fruityvice_response.json())
 #to loo
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Output in format of table
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 
 
